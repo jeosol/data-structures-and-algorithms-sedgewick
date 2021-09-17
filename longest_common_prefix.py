12aa16546e8680d6af306c816ad4810e317f791e
Jerome Onwunalu <jeronwunalu@gmail.com>
2021-09-16 21:09:36 -0500

    Add comments in function to explain code segments

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -3,21 +3,27 @@ from typing import Optional
 def longest_common_prefix(str1 : str, str2 : str) -> str:
 	"""Return the longest_common_prefix between two strings str1 and str2."""
 	
+	# Check that both arguments are of type string, otherwise return
 	if type(str1) != str or type(str2) != str:
 		return None
 		
+	# Get the length of the shortest string
 	min_len = min(len(str1), len(str2))
 	
-	end_index = 0
+	end_index = 0	
+	# move through both strings, if we get a match, increment pointer otherwise break
+	# out of the loop 
 	
 	while (end_index < min_len):
 		if str1[end_index] == str2[end_index]:
 			end_index += 1
 		else:
 			break
-			
+	
+	# return the string in the interval [0, end_index)
 	return str1[0:end_index]
 	
+# Test code	
 import unittest
 
 class TestLCP(unittest.TestCase):
